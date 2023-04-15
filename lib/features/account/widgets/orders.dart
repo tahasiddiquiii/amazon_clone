93f1775d1b28dcfafb91fb738ebd10ad40a21f5d
taha <siddiquitaha72@gmail.com>
2023-04-15 06:26:23 +0530

    viewing my orders

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -1,5 +1,8 @@
+import 'package:amazon_clone/common/widgets/loader.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/account/services/account_services.dart';
 import 'package:amazon_clone/features/account/widgets/single_product.dart';
+import 'package:amazon_clone/models/order.dart';
 import 'package:flutter/material.dart';
 
 class Orders extends StatefulWidget {
@@ -10,66 +13,74 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
-  // temporary List
-  List list = [
-    'https://images.unsplash.com/photo-1678933632171-f730be1aa086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-    'https://images.unsplash.com/photo-1678933632171-f730be1aa086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-    'https://images.unsplash.com/photo-1678933632171-f730be1aa086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-    'https://images.unsplash.com/photo-1678933632171-f730be1aa086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-  ];
+  List<Order>? orders;
+  final AccountServices accountServices = AccountServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
+
+  void fetchOrders() async {
+    orders = await accountServices.fetchMyOrders(context: context);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(
-                left: 15,
+    return orders == null
+        ? const Loader()
+        : Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                    ),
+                    child: const Text(
+                      'Your Orders',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      right: 15,
+                    ),
+                    child: Text(
+                      'See All',
+                      style: TextStyle(
+                        color: GlobalVariables.selectedNavBarColor,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              child: const Text(
-                'Your Orders',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(
-                right: 15,
-              ),
-              child: Text(
-                'See All',
-                style: TextStyle(
-                  color: GlobalVariables.selectedNavBarColor,
-                ),
-              ),
-            ),
-          ],
-        ),
-        // Display Orders
+              // Display Orders
 
-        Container(
-          height: 270,
-          padding: const EdgeInsets.only(
-            left: 10,
-            top: 20,
-            right: 0,
-          ),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: list.length,
-            itemBuilder: ((context, index) {
-              return SingleProduct(
-                image: list[index],
-              );
-            }),
-          ),
-        )
-      ],
-    );
+              Container(
+                height: 270,
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  top: 20,
+                  right: 0,
+                ),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: orders!.length,
+                  itemBuilder: ((context, index) {
+                    return SingleProduct(
+                      image: orders![index].products[0].images[0],
+                    );
+                  }),
+                ),
+              )
+            ],
+          );
   }
 }
