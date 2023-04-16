b3754b8cffe1eddec248806b0e980c7798300d6e
taha <siddiquitaha72@gmail.com>
2023-04-17 00:03:19 +0530

    admin viewing all orders

--- lib/features/admin/screens/admin_screen.dart
+++ lib/features/admin/screens/admin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/admin/screens/orders_screen.dart';
 import 'package:amazon_clone/features/admin/screens/post_screens.dart';
 import 'package:flutter/material.dart';
 
@@ -19,9 +20,7 @@ class _AdminScreenState extends State<AdminScreen> {
     const Center(
       child: Text('Analytics page'),
     ),
-    const Center(
-      child: Text('Cart page'),
-    ),
+    const OrdersScreen(),
   ];
 
   void updatePage(int page) {
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,28 +1,27 @@
-const express = require('express');
+const express = require("express");
 const adminRouter = express.Router();
-const admin = require('../middilewares/admin');
-const { Product } = require('../models/product');
-
+const admin = require("../middilewares/admin");
+const { Product } = require("../models/product");
+const Order = require("../models/order");
 
 // Add product
 adminRouter.post("/admin/add-product", admin, async (req, res) => {
-    try {
-      const { name, description, images, quantity, price, category } = req.body;
-      let product = new Product({
-        name,
-        description,
-        images,
-        quantity,
-        price,
-        category,
-      });
-      product = await product.save();
-      res.json(product);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-  });
-
+  try {
+    const { name, description, images, quantity, price, category } = req.body;
+    let product = new Product({
+      name,
+      description,
+      images,
+      quantity,
+      price,
+      category,
+    });
+    product = await product.save();
+    res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 // get all your products
 adminRouter.get("/admin/get-products", admin, async (req, res) => {
@@ -34,13 +33,21 @@ adminRouter.get("/admin/get-products", admin, async (req, res) => {
   }
 });
 
-
 // Delete the product
 adminRouter.post("/admin/delete-product", admin, async (req, res) => {
   try {
     const { id } = req.body;
     let product = await Product.findByIdAndDelete(id);
     res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+adminRouter.get("/admin/get-orders", admin, async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    res.json(orders);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
